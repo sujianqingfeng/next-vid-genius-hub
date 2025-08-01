530195448a3aa51f9ca702d59871dc6cec89cbb1
hens <henieshuang@wangxiaobao.com>
2025-08-02 00:24:32 +0800

    chore: Refactor code style across multiple files for consistency, update component imports, and enhance error handling in database configuration.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold text-center sm:text-left">
-          Welcome to Next.js
-        </h1>
-        <p className="text-lg text-center sm:text-left max-w-[600px]">
-          Your Next.js application is ready to go!
-        </p>
-      </main>
-    </div>
-  );
+	return (
+		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+				<h1 className="text-4xl font-bold text-center sm:text-left">
+					Welcome to Next.js
+				</h1>
+				<p className="text-lg text-center sm:text-left max-w-[600px]">
+					Your Next.js application is ready to go!
+				</p>
+			</main>
+		</div>
+	)
 }
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,16 +4,13 @@ import * as schema from "./schema";
 import { DATABASE_URL } from "../constants";
 
 if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing");
+	throw new Error("DATABASE_URL is missing");
 }
 
 const client = createClient({
-  url: DATABASE_URL,
+	url: DATABASE_URL,
 });
 
 const db = drizzle(client, { schema });
 
-export {
-  schema,
-  db,
-}
+export { schema, db };
