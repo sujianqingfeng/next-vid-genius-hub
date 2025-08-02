dffb07470f05bd7b0106a4fc89a57c78fd749b54
hens <henieshuang@wangxiaobao.com>
2025-08-02 10:04:38 +0800

    chore: Add @paralleldrive/cuid2 dependency, update database schema to include downloads table, and adjust VSCode settings for spell checking.

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,16 +1,16 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import * as schema from "./schema";
-import { DATABASE_URL } from "../constants";
+import { createClient } from '@libsql/client'
+import { drizzle } from 'drizzle-orm/libsql'
+import { DATABASE_URL } from '../constants'
+import * as schema from './schema'
 
 if (!DATABASE_URL) {
-	throw new Error("DATABASE_URL is missing");
+	throw new Error('DATABASE_URL is missing')
 }
 
 const client = createClient({
 	url: DATABASE_URL,
-});
+})
 
-const db = drizzle(client, { schema });
+const db = drizzle(client, { schema })
 
-export { schema, db };
+export { schema, db }
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,9 +1,23 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { createId } from '@paralleldrive/cuid2'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const videos = sqliteTable("videos", {
-	id: integer("id").primaryKey({ autoIncrement: true }),
-	title: text("title").notNull(),
-	url: text("url").notNull().unique(),
-	description: text("description"),
-	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
-});
+export const downloads = sqliteTable('downloads', {
+	id: text('id')
+		.unique()
+		.notNull()
+		.$defaultFn(() => createId()),
+	title: text('title').notNull(),
+	author: text('author'),
+	source: text('source', { enum: ['youtube', 'tiktok'] }).notNull(),
+	quality: text('quality', { enum: ['720p', '1080p'] }).notNull(),
+	thumbnail: text('thumbnail'),
+	viewCount: integer('view_count').default(0),
+	likeCount: integer('like_count').default(0),
+	commentCount: integer('comment_count').default(0),
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	downloadUrl: text('download_url').notNull(),
+	filePath: text('file_path'),
+	audioFilePath: text('audio_file_path'),
+})
